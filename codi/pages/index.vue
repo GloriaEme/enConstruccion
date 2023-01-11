4c7ceb731725b9b301047686acb89a821e89dc81
Gloria Molina <gloriamolina44@gmail.com>
2023-01-11 17:27:57 +0100

    foto

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -4,8 +4,7 @@
       <v-col align="center">
         <div style="font-weight:100;font-size:40px;color:white">
           <strong>En Contrucción</strong> 
-          <br>
-          Próximamente 
+          <v-img src="https://th.bing.com/th/id/OIP.ywoe9UsI3g2vLNUhhr-BaQHaE7?w=270&h=180&c=7&r=0&o=5&dpr=2.5&pid=1.7"></v-img> 
         </div>
       </v-col>
     </v-row>
